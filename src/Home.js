2778af3e9c044e7cb515119fca6da8801d339aee
BobbyCharms <robertcharming@Gmail.com>
2023-11-03 08:49:45 -0400

    hooks up json db to react app, handles for error messages

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,49 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Marin', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Peach', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    // ****************** Learning Material
+        // [{ title: 'My new website', body: 'lorem ipsum...', author: 'Marin', id: 1 },
+        // { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
+        // { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Peach', id: 3 }]
 
     const [name, setName] = useState('Marin');
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    // ****************** Learning Material
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
 
     useEffect(() => {
+        // Using setTimeout to simulate a delay in getting data from a server for simulation purposes
+        setTimeout(() => {
         console.log('use effect ran');
         // console.log(blogs);
-        console.log(name);
-    }, [name]);
+        // console.log(name);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                console.log(res);
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                console.log(err.message);
+                setIsPending(false);
+                setError(err.message);
+            });
+        }, 1000);
+    }, []);
+
     // ****************** Learning Material
     // let name = 'robert';
     // the [] is array destructuring; the first element is the value, the second is the function that updates the value
@@ -42,14 +67,15 @@ const Home = () => {
     // const handleClickAgain = (name, e) => {
     //     console.log('hello ' + name, e.target);
     
-
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
             <button onClick={() => setName('Bill')}>Change Name</button>
             <p>{ name }</p>
             
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Marin')} title="Marin's Blogs" handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'Marin')} title="Marin's Blogs"/>}
         {/* ****************** Learning Material */
             /* <h2>Homepage</h2>
            <p>{ name  is {age years old</p> *
