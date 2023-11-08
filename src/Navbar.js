e3e19d0b3efe13c95ae6e164cbda384385eb71fb
BobbyCharms <robertcharming@Gmail.com>
2023-11-08 05:43:45 -0500

    updates a tags to Link tags to avoid hitting the server

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,19 @@
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
                 {/* Dynamic value in JSX for styling, which is an object. Outside braces identify dynamic value, inside identify it as an object. */}
                 {/* Key value pairs are always the css property and then the value. The value is always a string. */}
                 {/* In JSX key value pair properties are camel case. No - signs */}
-                <a href="/create" style={{
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px"
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
       );
